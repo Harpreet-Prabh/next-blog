6bd6c78aeeac15093419a60ce8920893b78db4c8
Harpreet Kaur <harpreetrandhawwa@gmail.com>
2026-06-08 15:43:29 -0400

    Implement AuthLinks and ThemeToggle components with styles; add ThemeContext for theme management

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import styles from "./authLinks.modules.css";
+import Link from "next/link";
 
 export default function AuthLinks() {
-  return <div className={styles.container}>AuthLinks</div>;
+  //temporary status for testing
+  const status = "notauthenticated";
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  );
 }
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+export const ThemeContext = createContext();
+export const ThemeContextProvider = ({ children }) => {
+  const { theme, setTheme } = useState("light");
+  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+};
